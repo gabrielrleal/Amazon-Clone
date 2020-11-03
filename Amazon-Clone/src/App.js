253a3e8887af0631c114a775c2da7e112ab91042
Gabriel Leal <gabrielxleal@gmail.com>
2020-11-03 14:01:23 -0300

    Add product component

--- Amazon-Clone/src/App.js
+++ Amazon-Clone/src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
-
+import Checkout from "./Checkout";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
         <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
