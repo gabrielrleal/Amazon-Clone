e5a3960c2e3bbd127ecd74368f7d1c8ab2630a33
Gabriel Leal <gabrielxleal@gmail.com>
2020-11-06 17:18:38 -0300

    index

--- Amazon-Clone/src/index.js
+++ Amazon-Clone/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/pages/index.css";
+import "./styles/global/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
@@ -16,8 +16,5 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 serviceWorker.unregister();
