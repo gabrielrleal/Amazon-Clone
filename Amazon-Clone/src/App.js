08fad98d5ae698590df124b42836f9f6704ce32b
Gabriel Leal <gabrielxleal@gmail.com>
2020-11-06 16:47:42 -0300

    add routes

--- Amazon-Clone/src/App.js
+++ Amazon-Clone/src/App.js
@@ -1,26 +1,12 @@
 import "./styles/pages/App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
 import Header from "./components/Header";
-import Checkout from "./pages/Checkout";
-import Login from "./pages/Login";
+import Routes from "./routes";
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Router>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <Routes />
     </div>
   );
 }
--- Amazon-Clone/src/routes.js
+++ Amazon-Clone/src/routes.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-//switch faz com que apenas uma rota seja exibida em tela
-import Landing from "./pages/Landing";
-import OrphanagesMap from "./pages/OrphanagesMap";
-import CreateOrphanage from "./pages/CreateOrphanage";
-import Orphanage from "./pages/Orphanage";
-function Routes() {
-  // exact comparação de igualdade, geralmente colocada na primeira rota "/"
+import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
 
+function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Landing} />
-        <Route path="/app" component={OrphanagesMap} />
-        <Route path="/orphanages/create" component={CreateOrphanage} />
-        <Route path="/orphanages/:id" component={Orphanage} />
+        <Route path="/" exact component={Home} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/login" component={Login} />
       </Switch>
     </BrowserRouter>
   );
