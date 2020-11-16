8377c4a4c6d48336b525adae78530142c87f538a
Gabriel Leal <gabrielxleal@gmail.com>
2020-11-16 16:38:22 -0300

    deployed version

--- Amazon-Clone/src/axios.js
+++ Amazon-Clone/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/challenge-e5509/us-central1/api", //the api(cloud function) URL
+  baseURL: "https://us-central1-challenge-e5509.cloudfunctions.net/api",
+  // "http://localhost:5001/challenge-e5509/us-central1/api", // local
 });
 
 export default instance;
--- Amazon-Clone/src/components/CheckoutProduct.js
+++ Amazon-Clone/src/components/CheckoutProduct.js
@@ -6,7 +6,6 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
   const [{ basket }, dispatch] = useStateValue();
 
   const removeFromBasket = () => {
-    //remove the item from basket
     dispatch({
       type: "REMOVE_FROM_BASKET",
       id: id,
@@ -16,7 +15,6 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} alt="checkoutImage" />
-
       <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
         <p className="checkoutProduct__price">
